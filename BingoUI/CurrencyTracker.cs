d30569bb0a0af3d0a69a346690d3d91448446897
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-12-20 23:59:25 +0000

    Remove outdated code

--- BingoUI/CurrencyTracker.cs
+++ BingoUI/CurrencyTracker.cs
@@ -19,7 +19,6 @@ public static class CurrencyTracker
 
     internal static void Hook()
     {
-        // Md.CurrencyCounterBase.LateUpdate.Postfix(ModifyCurrencyText, manager: currencyMgr);
         Md.CurrencyCounter.Take.Postfix(RecordSpentCurrency, manager: currencyMgr);
         Md.CurrencyCounter.Awake.Postfix(AddCurrencyTrackers, manager: currencyMgr);
     }
@@ -29,13 +28,6 @@ public static class CurrencyTracker
         if (self.currencyType != CurrencyType.Money) return;  // Could count shards too...
 
         self.gameObject.AddComponent<CurrencyCounterModifier>();
-
-        /*
-        if (self.geoTextMesh.tmpText is not TMProOld.TMP_Text text) return;
-        text.fontSize *= 0.6f;
-        self.initialAddTextX += 0.7f;
-        self.initialSubTextX += 0.7f;
-        */
     }
 
     private static void RecordSpentCurrency(ref int amount, ref CurrencyType type)
@@ -48,27 +40,5 @@ public static class CurrencyTracker
     internal static int GetCurrencySpent(CurrencyType type)
     {
         return SaveData.Instance.SpentCurrency.TryGetValue(type, out int spent) ? spent : 0;
-    }
-
-    private static void ModifyCurrencyText(CurrencyCounterBase self)
-    {
-        if (self is not CurrencyCounter ctr) return;
-        if (ctr.currencyType != CurrencyType.Money) return;
-
-        string spentGeoString;
-        try
-        {
-            spentGeoString = string.Format(
-                Language.Get("SPENT_GEO_FMT", $"Mods.{BingoUIPlugin.Id}"),
-                GetCurrencySpent(ctr.CounterType)
-            );
-        }
-        catch (Exception)
-        {
-            // If a localizer didn't put {0} in the string I don't want to blow up the mod
-            spentGeoString = $"{GetCurrencySpent(ctr.CounterType)} spent";
-        }
-        
-        ctr.geoTextMesh.Text = $"{self.counterCurrent}\n({spentGeoString})";
     }
 }
